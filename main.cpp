f97385540c16685c0d9674dc1afda98ddf2bc47e
g.pimenov <aquacreatura@gmail.com>
2026-06-17 11:56:39 +0300

    ci(TC): Добавлена поддержка запуска из Total Commander

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "AquaWave.h"
 #include <QtWidgets/QApplication>
-#include <thread>
+
+#include <qdir.h>
+#include <Windows.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -8,8 +10,17 @@ int main(int argc, char *argv[])
     if(!cur_icon.isNull())
         a.setWindowIcon(cur_icon);
     QString file_path;
-    if(argc >= 2)
-        file_path = QString::fromLocal8Bit(argv[1]);
+	if (argc >= 2) {
+		file_path = QString::fromLocal8Bit(argv[1]);
+		
+		{// 2. Стучимся в Total Commander за активным путем
+			wchar_t b[512] = { 0 };
+			GetWindowTextW((HWND)SendMessage(FindWindowA("TTOTAL_CMD", nullptr), 1074, 21, 0), b, 512);
+			QString tcDir = QString::fromWCharArray(b).split('>')[0].remove('*').trimmed();
+			// 3. Если путь из TC получен — склеиваем его с именем файла, иначе — берем дефолтный абсолютный путь
+			file_path = tcDir.isEmpty() ? QFileInfo(file_path).absoluteFilePath() : QDir(tcDir).absoluteFilePath(file_path);
+		}
+	}
     AquaWave w(nullptr, file_path);
     w.show();
     return a.exec();
